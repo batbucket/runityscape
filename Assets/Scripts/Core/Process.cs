202c8309b9097ddf782f49609ca1d7ddb3b45ffc
Atra <sharr@derpymail.org>
2015-12-29 07:41:37 -0500

    Interface has been implemented. Still need to test

---
+++ Assets/Scripts/Core/Process.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class Process : IProcess {
+    Action playAction;
+    Action undoAction;
+
+    public void setPlay(Action action) {
+        playAction = action;
+    }
+
+    public void setUndo(Action action) {
+        undoAction = action;
+    }
+
+    public void play() {
+        playAction.Invoke();
+    }
+
+    public void undo() {
+        undoAction.Invoke();
+    }
+}
--- Assets/Scripts/Misc/Testy.cs
+++ Assets/Scripts/Misc/Testy.cs
@@ -3,12 +3,22 @@ using System.Collections;
 
 public class Testy : MonoBehaviour {
 
-    public const string testy = "a b c d e f g h i j k l m n o p 1 2 3 4 5 6 7 8 ";
+    HeaderManager header;
+    TimeManager time;
+    TextBoxHolderManager textBoxes;
+    ActionGridManager actionButtons;
+    PortraitHolderManager leftPortraits;
+    PortraitHolderManager rightPortraits;
 
     // Use this for initialization
     void Start() {
-        TextBoxManager t = gameObject.GetComponentInChildren<TextBoxManager>();
-        t.post(testy, .01f);
+        header = GameObject.Find("HeaderHolder").GetComponent<HeaderManager>();
+        time = GameObject.Find("TimeHolder").GetComponent<TimeManager>();
+        textBoxes = GameObject.Find("TextBoxHolder").GetComponent<TextBoxHolderManager>();
+        actionButtons = GameObject.Find("ButtonHolder").GetComponent<ActionGridManager>();
+        leftPortraits = GameObject.Find("LeftPortraitHolder").GetComponent<LeftPortraitHolderManager>();
+        rightPortraits = GameObject.Find("RightPortraitHolder").GetComponent<RightPortraitHolderManager>();
+        //actionButtons.setButtonAttributes
     }
 
     // Update is called once per frame
