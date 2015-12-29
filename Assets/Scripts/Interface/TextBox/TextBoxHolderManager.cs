5f39b9c383b25c052432d04254ed3852ac393d8c
Atra <sharr@derpymail.org>
2015-12-29 07:22:20 -0500

    Implement TextBoxHolderManager

--- Assets/Scripts/Interface/TextBox/TextBoxHolderManager.cs
+++ Assets/Scripts/Interface/TextBox/TextBoxHolderManager.cs
@@ -1,14 +1,9 @@
 ﻿using UnityEngine;
 
 public class TextBoxHolderManager : MonoBehaviour {
-
-    // Use this for initialization
-    void Start() {
-
-    }
-
-    // Update is called once per frame
-    void Update() {
-
+    public TextBoxManager addTextBox() {
+        GameObject g = (GameObject)GameObject.Instantiate(Resources.Load("TextBox"));
+        Util.parent(g, gameObject);
+        return g.GetComponent<TextBoxManager>();
     }
 }
