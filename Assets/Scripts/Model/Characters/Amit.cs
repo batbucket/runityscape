b9a922f633950ff073acab86ac967412981f65dc
ubitquitin <khopkar.rohan@gmail.com>
2016-01-06 00:45:22 -0600

    Created Item class and derived class- NotImplemented Exception in Lobster

--- Assets/Scripts/Model/Characters/Amit.cs
+++ Assets/Scripts/Model/Characters/Amit.cs
@@ -6,7 +6,8 @@ public class Amit : PlayerCharacter {
     public Amit() : base(Util.getSprite("crying_mudkip"), "Amit", 0, 5, 5, 5, 5) {
         addResources(ResourceFactory.createResource(ResourceType.SKILL, 3));
         fightSpells.Add("Attack");
-        fightSpells.Add("Meditate");
+       // fightSpells.Add("Meditate");
+        fightSpells.Add("Lobster");
     }
 
     public override bool isDefeated(Game game) {
