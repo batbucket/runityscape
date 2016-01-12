402c91b9a860da5e87c018e44daeea327c40f769
Atra <sharr@derpymail.org>
2016-01-12 00:39:19 -0500

    New Equipment class to represent what the player is wearing

---
+++ Assets/Scripts/Model/Items/Equipment.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Equipment {
+    Dictionary<string, EquippableItem> equipment;
+
+	public Equipment() {
+        equipment = new Dictionary<string, EquippableItem>();
+        equipment.Add("Weapon", null);
+        equipment.Add("Armor", null);
+        equipment.Add("Trinket", null);
+	}
+}
