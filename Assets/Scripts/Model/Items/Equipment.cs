94f95707c29ffacc9fb9b2f3b9a94d5349661e41
Atra <sharr@derpymail.org>
2016-01-15 01:05:13 -0500

    Added method headers to Equipment

--- Assets/Scripts/Model/Items/Equipment.cs
+++ Assets/Scripts/Model/Items/Equipment.cs
@@ -3,13 +3,25 @@ using System.Collections;
 using System.Collections.Generic;
 
 public class Equipment {
-    Dictionary<EquipmentType, EquippableItem> equipment;
+    SortedDictionary<EquipmentType, EquippableItem> equipment;
 
 	public Equipment() {
-        equipment = new Dictionary<EquipmentType, EquippableItem>();
+        equipment = new SortedDictionary<EquipmentType, EquippableItem>();
         equipment.Add(EquipmentType.WEAPON, null);
         equipment.Add(EquipmentType.OFFHAND, null);
         equipment.Add(EquipmentType.ARMOR, null);
         equipment.Add(EquipmentType.TRINKET, null);
+    }
+
+    public Equipment equip(EquipmentType equipmentType, EquippableItem equippableItem) {
+        return null;
+    }
+
+    public Equipment unequip(EquipmentType equipmentType) {
+        return null;
+    }
+
+    public bool hasEquipment(EquipmentType equipmentType) {
+        return false;
     }
 }
