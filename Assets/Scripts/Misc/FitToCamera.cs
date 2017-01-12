48007cc185c749a20d7212e822690a3918ea3a05
Atra <sharr@derpymail.org>
2017-01-12 03:03:15 -0500

    Game no longer cuts off at 19:10 aspect ratios. Still need some anchoring done.

---
+++ Assets/Scripts/Misc/FitToCamera.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Scales a recttransform to fit the camera dimensions.
+/// </summary>
+public class FitToCamera : MonoBehaviour {
+
+    [SerializeField]
+    private RectTransform target;
+
+    /// <summary>
+    /// http://answers.unity3d.com/questions/640325/get-screen-height-in-real-world-coordinates.html
+    /// </summary>
+    private void Start() {
+        float height = Camera.main.orthographicSize * 2.0f;
+        float width = height * Camera.main.aspect;
+
+        target.sizeDelta = new Vector2(width, height);
+    }
+
+}
