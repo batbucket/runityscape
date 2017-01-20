49be8adcab87b745ed183e0de2dceb0895930cb0
Atra <sharr@derpymail.org>
2017-01-20 03:15:38 -0500

    Added new hotkey test page

---
+++ Assets/Scripts/Model/Game/NonGameplay/HotkeyTest.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using Scripts.Model.Pages;
+using Scripts.View.ActionGrid;
+using Scripts.Model.Processes;
+using Scripts.Presenter;
+using Scripts.Model.TextBoxes;
+
+namespace Scripts.Model.World.Pages {
+
+    public class HotkeyTest : ReadPage {
+        public HotkeyTest(Page back) : base(location: "Hotkey Test", tooltip: "Press TAB to leave.") {
+            for (int i = 0; i < ActionGridView.TOTAL_BUTTON_COUNT; i++) {
+                string hotkey = ActionGridView.HOTKEYS[i].ToString();
+                ActionGrid[i] = new Process(
+                        hotkey,
+                        "",
+                        () => Game.Instance.TextBoxes.AddTextBox(new TextBox(hotkey))
+                    );
+            }
+
+            OnTickAction += () => {
+                if (Input.GetKeyDown(KeyCode.Tab)) {
+                    Game.Instance.CurrentPage = back;
+                }
+            };
+        }
+    }
+}
