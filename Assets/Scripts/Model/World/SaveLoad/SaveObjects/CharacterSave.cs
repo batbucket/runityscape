52ecfaf0f204b338f63182ff091797b9c28e7e74
Atra <sharr@derpymail.org>
2017-06-09 01:39:06 -0400

    These aren't too useful right now since a large part of their code has been removed to avoid conflicts with the overhauls occurring.

---
+++ Assets/Scripts/Model/World/SaveLoad/SaveObjects/CharacterSave.cs
@@ -0,0 +1,24 @@
+﻿using Scripts.Model.Characters;
+using System.Linq;
+
+namespace Scripts.Model.World.Serialization.SaveObject {
+
+    [System.Serializable]
+    public struct CharacterSave : IRestorable<Character> {
+        public AttributeListSave Attributes;
+        public string Name;
+        public string Type;
+
+        public CharacterSave(Character c) {
+            Type = Util.GetClassName(c);
+            Name = c.Look.Name;
+            Attributes = new AttributeListSave(c.Stats.Dict.Values.ToList());
+        }
+
+        public Character Restore() {
+            Character c = Util.StringToObject<Character>(Type);
+            c.Look.Name = this.Name;
+            return c;
+        }
+    }
+}
---
+++ Assets/Scripts/Model/World/SaveLoad/SavePage.cs
@@ -0,0 +1,20 @@
+﻿using Scripts.Model.Interfaces;
+using Scripts.Model.Pages;
+using Scripts.Model.Processes;
+using Scripts.Model.TextBoxes;
+using Scripts.View.ActionGrid;
+using Scripts.Presenter;
+using System;
+
+namespace Scripts.Model.World.Serialization {
+
+    /// <summary>
+    /// Page that allows users to save and overwrite data.
+    /// </summary>
+    public class SavePage : Page {
+
+        public SavePage() : base("Save") {
+            Body = "Make a new save, or overwrite an existing one.";
+        }
+    }
+}
