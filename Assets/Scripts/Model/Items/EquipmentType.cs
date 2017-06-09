9667625089765e6c83ca771e7fd61d07cfba8489
Atra <sharr@derpymail.org>
2017-06-09 01:34:53 -0400

    Items have been removed temporarily due to conflicts with the overhauls.

--- Assets/Scripts/Model/Items/EquipmentType.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Scripts.Model.Items {
-
-    /// <summary>
-    /// The various types of equipment.
-    /// </summary>
-    public enum EquipmentType {
-        WEAPON, OFFHAND, ARMOR, TRINKET
-    }
-}
