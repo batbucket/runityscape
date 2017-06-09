c9f1d9b8df07a9b8de70f0f28b644d6fa9866baf
Atra <sharr@derpymail.org>
2017-06-09 01:47:08 -0400

    Targeting overhaul.

--- Assets/Scripts/Model/Spells/TargetType.cs
+++ Assets/Scripts/Model/Spells/TargetType.cs
@@ -1,9 +1,38 @@
-﻿namespace Scripts.Model.Spells {
+﻿using Scripts.Model.Characters;
+using Scripts.Model.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Scripts.Model.Spells {
 
     /// <summary>
     /// Who a Spell can target.
     /// </summary>
-    public enum TargetType {
-        SELF, SINGLE_ALLY, ALL_ALLY, SINGLE_ENEMY, ALL_ENEMY, ANY, ALL
+    public sealed class TargetType {
+        //SELF, SINGLE_ALLY, ALL_ALLY, SINGLE_ENEMY, ALL_ENEMY, ANY, ALL
+
+        public readonly string Name;
+        private Func<Character, Page, ICollection<Character>> getFunc;
+
+        private TargetType(string name, Func<Character, Page, ICollection<Character>> getFunc) {
+            this.Name = name;
+            this.getFunc = getFunc;
+        }
+
+        public static TargetType SELF = new TargetType("Self", (c, p) => new Character[] { c });
+        public static TargetType SINGLE_ALLY = new TargetType("Single ally", (c, p) => p.GetAllies(c).Where(a => a.Stats.State == State.ALIVE).ToArray());
+        public static TargetType ALL_ALLIES = new TargetType("All allies", (c, p) => p.GetAllies(c).Where(a => a.Stats.State == State.ALIVE).ToArray());
+        public static TargetType SINGLE_ENEMY = new TargetType("Single enemy", (c, p) => p.GetFoes(c).Where(a => a.Stats.State == State.ALIVE).ToArray());
+        public static TargetType ALL_ENEMIES = new TargetType("All enemies", (c, p) => p.GetFoes(c).Where(a => a.Stats.State == State.ALIVE).ToArray());
+        public static TargetType ANY = new TargetType("Any", (c, p) => p.GetAll().Where(a => a.Stats.State == State.ALIVE).ToArray());
+        public static TargetType ALL = new TargetType("All", (c, p) => p.GetAll().Where(a => a.Stats.State == State.ALIVE).ToArray());
+
+        public static HashSet<TargetType> SINGLE_TARGET_OPTIONS = new HashSet<TargetType> { SELF, SINGLE_ALLY, SINGLE_ENEMY, ANY };
+
+        public ICollection<Character> GetTargets(Character caster, Battle b) {
+            return getFunc.Invoke(caster, b);
+        }
+
     }
 }
