2a74d32671c81213d4d502801e611dbce27afd8a
Atra <sharr@derpymail.org>
2017-06-09 01:34:23 -0400

    Page system overhaul.

--- Assets/Scripts/Model/Page/BattleState.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Scripts.Model.Pages {
-
-    /// <summary>
-    /// Denotes the state of the battle.
-    /// </summary>
-    public enum BattleState {
-        PRE_BATTLE, BATTLE, VICTORY, DEFEAT, POST_VICTORY, POST_DEFEAT
-    }
-}
