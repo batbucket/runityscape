675be9a8cbdeec3dbe008c53b4c2da5e31fa3b66
Atra <sharr@derpymail.org>
2017-06-27 00:02:33 -0400

    Cutscene code improved.

--- Assets/Scripts/Model/Events/Act.cs
+++ Assets/Scripts/Model/Events/Act.cs
@@ -11,44 +11,17 @@ using System.Text.RegularExpressions;
 using UnityEngine;
 
 namespace Scripts.Model.Acts {
-    public static class Util {
+    public static class ActUtil {
         private const char TEXTBOX_SYMBOL = 't';
         private const char AVATARBOX_SYMBOL = 'a';
-        private static readonly string REGEX = string.Format("((<({0}|{1})>)(?:(?!(<({0}|{1})>)).)*)", TEXTBOX_SYMBOL, AVATARBOX_SYMBOL); //"((<.>)(?:(?!(<.>)).)*)";
+        private static readonly string SYMBOL_TAG_DETECTION = string.Format("((<({0}|{1})>)(?:(?!(<({0}|{1})>)).)*)", TEXTBOX_SYMBOL, AVATARBOX_SYMBOL); //"((<.>)(?:(?!(<.>)).)*)";
         private const int IDENTIFIER_POSITION = 1;
         private const int TAG_LENGTH = 3;
 
-        public static IEnumerator SetupScene(Page page, params Act[] acts) {
-            IButtonable[] originalArray = page.Actions;
-
-            bool isSkipAll = false;
-
-            int lastIndex = 0;
-
-            for (int i = 0; i < acts.Length && !isSkipAll; i++) {
-                lastIndex = i;
-                bool isStepped = false;
-                Act act = acts[i];
-                page.Actions = new IButtonable[] {
-                    new Process("Skip", "Skip the current event.", () => act.SkipAct())
-                };
-                yield return act.Play();
-                page.Actions = new IButtonable[] {
-                    new Process("Step", "Advance to the next event.", () => isStepped = true),
-                    new Process("Skip All", "Skip the current scene.", () => isSkipAll = true)
-                };
-                yield return new WaitUntil(() => isStepped || i == (acts.Length - 1) || isSkipAll);
-            }
-            if (isSkipAll) {
-                for (int i = lastIndex + 1; i < acts.Length; i++) {
-                    Act act = acts[i];
-                    act.SkipAct();
-                    yield return act.Play();
-                }
-            }
-
-            page.Actions = originalArray;
+        public static void SetupScene(Page page, params Act[] acts) {
+            Main.Instance.StartCoroutine(SetupSceneRoutine(page, acts));
         }
+
         public static Act[] LongTalk(Page p, Character c, string big) {
             List<Act> events = new List<Act>();
             IList<TextBox> boxes = SplitBigDialogue(p, c, big);
@@ -59,13 +32,51 @@ namespace Scripts.Model.Acts {
             return events.ToArray();
         }
 
+        private static IEnumerator SetupSceneRoutine(Page page, Act[] acts) {
+            Grid grid = new Grid(string.Empty);
+            Main.Instance.PagePresenter.Override = grid;
+
+            bool isSkipAll = false;
+            int lastIndex = 0;
+
+            for (int i = 0; i < acts.Length && !isSkipAll; i++) {
+                lastIndex = i;
+                bool isStepped = false;
+                Act act = acts[i];
+                grid.Array = new IButtonable[] {
+                    new Process("Skip", "Skip the current event.", () => act.SkipAct()),
+                    null
+                };
+                yield return act.Play();
+                if (i < acts.Length - 1) {
+                    grid.Array = new IButtonable[] {
+                    new Process("Step", "Advance to the next event.", () => isStepped = true),
+                    new Process("Skip All", "Skip the current scene.", () => isSkipAll = true)
+                };
+                }
+                yield return new WaitUntil(() => isStepped || i == (acts.Length - 1) || isSkipAll);
+            }
+            if (isSkipAll) {
+                for (int i = lastIndex + 1; i < acts.Length; i++) {
+                    Act act = acts[i];
+                    act.SkipAct();
+                    yield return act.Play();
+                }
+            }
+
+            // If grid we're using is still in use, wipe it
+            if (Main.Instance.PagePresenter.Override == grid) {
+                Main.Instance.PagePresenter.Override = null;
+            }
+        }
+
         /// <summary>
         /// Use regex to split up a long string into an Emotes
         /// and Talks cutscene by a specific character
         /// </summary>
         private static IList<TextBox> SplitBigDialogue(Page p, Character c, string big) {
             List<string> matches = new List<string>();
-            foreach (Match m in Regex.Matches(big, REGEX)) {
+            foreach (Match m in Regex.Matches(big, SYMBOL_TAG_DETECTION)) {
                 matches.Add(m.Value);
             }
             IList<TextBox> boxes = new List<TextBox>();
@@ -127,6 +138,10 @@ namespace Scripts.Model.Acts {
             this.box = box;
         }
 
+        public TextAct(string message) {
+            this.box = new TextBox(message);
+        }
+
         protected override IEnumerator PlayHelper() {
             Page.TypeText(box);
             while (!box.IsDone) {
@@ -136,6 +151,23 @@ namespace Scripts.Model.Acts {
 
         protected override void Skip() {
             box.Skip();
+        }
+    }
+
+    public class ActionAct : Act {
+        private Action action;
+
+        public ActionAct(Action action) {
+            this.action = action;
+        }
+
+        protected override IEnumerator PlayHelper() {
+            action.Invoke();
+            yield break;
+        }
+
+        protected override void Skip() {
+
         }
     }
 }
