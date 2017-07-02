7d988528f5566dbc2ce1424c588c63a4a293ab17
Atra <sharr@derpymail.org>
2017-07-02 05:58:07 -0400

    New helper class for quickly making spellbooks that have the following effect: put a buff on a target

---
+++ Assets/Scripts/Model/Spells/BuffAdder.cs
@@ -0,0 +1,33 @@
+﻿using Scripts.Game.Defined.Spells;
+using Scripts.Model.Buffs;
+using System;
+using System.Collections.Generic;
+
+namespace Scripts.Model.Spells {
+    public abstract class BuffAdder : BasicSpellbook {
+        private Buff dummy;
+
+        public BuffAdder(TargetType target, SpellType spell, Buff dummy, string name) : base(name, dummy.Sprite, target, spell) {
+            this.dummy = dummy;
+        }
+
+        protected sealed override IList<SpellEffect> GetHitEffects(SpellParams caster, SpellParams target) {
+            return new SpellEffect[] {
+                new AddBuff(
+                    new BuffParams(caster.Stats, caster.CharacterId),
+                    target.Buffs,
+                    Util.TypeToObject<Buff>(dummy.GetType()))
+            };
+        }
+
+        public sealed override string CreateDescriptionHelper(SpellParams caster) {
+            return string.Format(
+                "Target is affected by\n{0}\n{1}\nDuration: {2} turns.",
+                dummy.Name,
+                dummy.Description,
+                dummy.DurationText
+                );
+        }
+
+    }
+}
