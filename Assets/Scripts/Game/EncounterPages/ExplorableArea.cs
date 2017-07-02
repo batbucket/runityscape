ca0e0134dc5d5ae1561f93c4de62f7427bfb70e7
Atra <sharr@derpymail.org>
2017-07-02 07:47:24 -0400

    Moved class out of file.

---
+++ Assets/Scripts/Game/EncounterPages/ExplorableArea.cs
@@ -0,0 +1,57 @@
+﻿using Scripts.Game.Serialized;
+using Scripts.Model.Characters;
+using Scripts.Model.Pages;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Scripts.Game.Pages {
+    public abstract class ExplorableArea {
+        public readonly Explore Type;
+
+        protected Flags flags;
+        private Page camp;
+        private Party party;
+        private string location;
+        private PageGenerator generator;
+
+        public ExplorableArea(Explore type, Page camp, Flags flags, Party party, string location, Sprite sprite, string description) {
+            this.Type = type;
+            this.camp = camp;
+            this.flags = flags;
+            this.party = party;
+            this.location = location;
+            this.generator = new PageGenerator(location, sprite, description);
+            SetupEncounters();
+        }
+
+        public PageGenerator Generator {
+            get {
+                return generator;
+            }
+        }
+
+        protected abstract IEnumerable<Encounter> GetEncounters();
+
+        private void SetupEncounters() {
+            foreach (Encounter e in GetEncounters()) {
+                generator.AddEncounter(e);
+            }
+        }
+
+        protected Encounter RandomBattle(Rarity rarity, params Character[] enemies) {
+            return new Encounter(rarity, GetPageFunc(enemies));
+        }
+
+        private Func<Page> GetPageFunc(IEnumerable<Character> enemies) {
+            return () =>
+            new Battle
+                (camp,
+                camp,
+                Music.NORMAL,
+                location,
+                party.Collection,
+                enemies);
+        }
+    }
+}
--- Assets/Scripts/Game/EncounterPages/Specific/ExploreList.cs
+++ Assets/Scripts/Game/EncounterPages/Specific/ExploreList.cs
@@ -7,54 +7,6 @@ using System.Collections.Generic;
 using UnityEngine;
 
 namespace Scripts.Game.Pages.Explorables {
-    public abstract class ExplorableArea {
-        public readonly Explore Type;
-
-        protected Flags flags;
-        private Page camp;
-        private Party party;
-        private string location;
-        private PageGenerator generator;
-
-        public ExplorableArea(Explore type, Page camp, Flags flags, Party party, string location, Sprite sprite, string description) {
-            this.Type = type;
-            this.camp = camp;
-            this.flags = flags;
-            this.party = party;
-            this.location = location;
-            this.generator = new PageGenerator(location, sprite, description);
-            SetupEncounters();
-        }
-
-        public PageGenerator Generator {
-            get {
-                return generator;
-            }
-        }
-
-        protected abstract IEnumerable<Encounter> GetEncounters();
-
-        private void SetupEncounters() {
-            foreach (Encounter e in GetEncounters()) {
-                generator.AddEncounter(e);
-            }
-        }
-
-        protected Encounter RandomBattle(Rarity rarity, params Character[] enemies) {
-            return new Encounter(rarity, GetPageFunc(enemies));
-        }
-
-        private Func<Page> GetPageFunc(IEnumerable<Character> enemies) {
-            return () =>
-            new Battle
-                (camp,
-                camp,
-                Music.NORMAL,
-                location,
-                party.Collection,
-                enemies);
-        }
-    }
     public class Ruins : ExplorableArea {
         public Ruins(
             Page camp,
@@ -75,7 +27,7 @@ namespace Scripts.Game.Pages.Explorables {
 
         protected override IEnumerable<Encounter> GetEncounters() {
             return new Encounter[] {
-                RandomBattle(Rarity.COMMON, null)
+                RandomBattle(Rarity.COMMON,null)
             };
         }
     }
