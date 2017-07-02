0f75750017d9fc289ddf7ed99efb770aa944503e
Atra <sharr@derpymail.org>
2017-07-02 05:51:20 -0400

    Wait no longer requires the spell to be used.

--- Assets/Scripts/Game/Defined/Serialized/SpellList.cs
+++ Assets/Scripts/Game/Defined/Serialized/SpellList.cs
@@ -20,7 +20,8 @@ namespace Scripts.Game.Defined.Serialized.Spells {
         public const int SKILL_ON_HIT = 1;
         public const int SKILL_ON_CRIT = 2;
 
-        public Attack() : base("Attack", Util.GetSprite("gladius"), TargetType.SINGLE_ENEMY, SpellType.OFFENSE) { }
+        public Attack() : base("Attack", Util.GetSprite("gladius"), TargetType.SINGLE_ENEMY, SpellType.OFFENSE) {
+        }
 
         public override string CreateDescriptionHelper(SpellParams caster) {
             return string.Format("A basic attack with the weapons or fists.");
@@ -54,7 +55,9 @@ namespace Scripts.Game.Defined.Serialized.Spells {
     }
 
     public class Wait : BasicSpellbook {
-        public Wait() : base("Wait", Util.GetSprite("hourglass"), TargetType.SELF, SpellType.MERCY) { }
+        public Wait() : base("Wait", Util.GetSprite("hourglass"), TargetType.SELF, SpellType.MERCY) {
+            this.flags.Remove(Model.Spells.Flag.CASTER_REQUIRES_SPELL);
+        }
 
         public override string CreateDescriptionHelper(SpellParams caster) {
             return "Literally do nothing.";
@@ -65,30 +68,33 @@ namespace Scripts.Game.Defined.Serialized.Spells {
         }
     }
 
-    public class Check : BasicSpellbook {
-        public Check() : base("Check", Util.GetSprite("magnifying-glass"), TargetType.SINGLE_ENEMY, SpellType.OFFENSE) { }
+    public class Check : BuffAdder {
+        public Check() : base(TargetType.SINGLE_ENEMY, SpellType.OFFENSE, new Checked(), "Check") { }
+    }
 
-        public override string CreateDescriptionHelper(SpellParams caster) {
-            Checked dummy = new Checked();
-            return string.Format("Closely analyze an enemy, temporarily increasing their resource visibility.", dummy.Name, dummy.Description);
-        }
-
-        protected override IList<SpellEffect> GetHitEffects(SpellParams caster, SpellParams target) {
-            return new SpellEffect[] { new AddBuff(new BuffParams(caster.Stats, caster.CharacterId), target.Buffs, new Checked()) };
+    public class InflictPoison : BuffAdder {
+        public InflictPoison() : base(TargetType.SINGLE_ENEMY, SpellType.OFFENSE, new Poison(), "Infect") {
+            Costs.Add(StatType.MANA, 1);
         }
     }
 
-    public class InflictPoison : BasicSpellbook {
-        public InflictPoison() : base("Poison", Util.GetSprite("fox-head"), TargetType.SINGLE_ENEMY, SpellType.OFFENSE) {
-            Costs.Add(StatType.MANA, 1);
+    public class SetupCounter : BuffAdder {
+        public SetupCounter() : base(TargetType.SELF, SpellType.DEFENSE, new Counter(), "Counter") {
+            Costs.Add(StatType.SKILL, 1);
         }
+    }
+
+    public class Heal : BasicSpellbook {
+        public Heal() : base("Heal", Util.GetSprite("health-normal"), TargetType.SINGLE_ALLY, SpellType.BOOST) { }
 
         public override string CreateDescriptionHelper(SpellParams caster) {
-            return string.Format("Enemy is afflicted with Poison.");
+            return "Restore a little health.";
         }
 
         protected override IList<SpellEffect> GetHitEffects(SpellParams caster, SpellParams target) {
-            return new SpellEffect[] { new AddBuff(new BuffParams(caster.Stats, caster.CharacterId), target.Buffs, new Poison()) };
+            return new SpellEffect[] {
+                new AddToModStat(target.Stats, StatType.HEALTH, 5)
+            };
         }
     }
 }
