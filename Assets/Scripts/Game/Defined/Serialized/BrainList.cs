d46ce7058deba69edf4465541ac4391b927e1fac
Atra <sharr@derpymail.org>
2017-07-02 05:54:14 -0400

    Simplified enemy AI derived class: PriorityBrain Goes down a list, if any action is doable as it goes down, it performs it.

--- Assets/Scripts/Game/Defined/Serialized/BrainList.cs
+++ Assets/Scripts/Game/Defined/Serialized/BrainList.cs
@@ -9,35 +9,64 @@ using Scripts.Model.Characters;
 using Scripts.Game.Defined.Spells;
 using Scripts.Model.Items;
 using Scripts.Game.Defined.Serialized.Spells;
+using Scripts.Model.Stats;
 
 namespace Scripts.Game.Defined.Serialized.Characters {
 
     public class Player : Brain {
         public static readonly Attack Attack = new Attack();
-        private Battle battle;
 
-        public override void DetermineAction(Action<IPlayable> addPlay) {
+        public override void DetermineAction() {
             Grid main = new Grid("Main");
-            SpellParams ownerParams = new SpellParams(Owner);
             main.List = new IButtonable[] {
-                PageUtil.GenerateTargets(battle, main, ownerParams, Attack, addPlay),
-                PageUtil.GenerateSpellBooks(battle, main, ownerParams, Attack, addPlay),
-                PageUtil.GenerateItems(battle, main, ownerParams, addPlay),
-                PageUtil.GenerateEquipmentGrid(main, ownerParams, addPlay)
+                PageUtil.GenerateTargets(battle, main, Owner, Attack, handlePlay),
+                PageUtil.GenerateSpellBooks(battle, main, Owner, Attack, handlePlay),
+                PageUtil.GenerateItems(battle, main, Owner, handlePlay),
+                PageUtil.GenerateEquipmentGrid(main, Owner, handlePlay)
             };
             battle.Actions = main.List;
-        }
-
-        protected override void PageSetupHelper(Battle battle) {
-            this.battle = battle;
         }
     }
 
     public class DebugAI : Brain {
         public static readonly Attack Attack = new Attack();
 
-        public override void DetermineAction(Action<IPlayable> addPlay) {
+        public override void DetermineAction() {
             //addPlay(Spells.CreateSpell(Attack, Owner, foes.PickRandom()));
+        }
+    }
+
+    public class VillagerBrain : PriorityBrain {
+        public static readonly Attack Attack = new Attack();
+
+        protected override IList<Func<bool>> SetupPriorityActions() {
+            return new Func<bool>[] {
+                CastOnRandom(Attack)
+            };
+        }
+    }
+
+    public class KnightBrain : PriorityBrain {
+        public static readonly Attack Attack = new Attack();
+        public static readonly SetupCounter Counter = new SetupCounter();
+
+        protected override IList<Func<bool>> SetupPriorityActions() {
+            return new Func<bool>[] {
+                CastOnRandom(Counter),
+                CastOnRandom(Attack)
+            };
+        }
+    }
+
+    public class HealerBrain : PriorityBrain {
+        public static readonly Attack Attack = new Attack();
+        public static readonly Heal Heal = new Heal();
+
+        protected override IList<Func<bool>> SetupPriorityActions() {
+            return new Func<bool>[] {
+                CastOnTargetMeetingCondition(Heal, c => c.Stats.GetMissingStatCount(StatType.HEALTH) > 0),
+                CastOnRandom(Attack)
+            };
         }
     }
 }
---
+++ Assets/Scripts/Model/Characters/PriorityBrain.cs
@@ -0,0 +1,54 @@
+﻿using Scripts.Model.Characters;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using Scripts.Model.Spells;
+using System.Linq;
+using Scripts.Game.Defined.Serialized.Spells;
+
+namespace Scripts.Model.Characters {
+    public abstract class PriorityBrain : Brain {
+        private static readonly SpellBook DEFAULT_ACTION = new Wait();
+
+        private IList<Func<bool>> priorityActionList;
+
+        public PriorityBrain() : base() {
+            this.priorityActionList = SetupPriorityActions();
+        }
+
+        public sealed override void DetermineAction() {
+            bool hasChosenAction = false;
+            for (int i = 0; i < priorityActionList.Count && !hasChosenAction; i++) {
+                Func<bool> possibleAction = priorityActionList[i];
+                hasChosenAction = possibleAction();
+            }
+            // Unable to find anything, if we don't do anything here Battle will get stuck on computer thinking
+            handlePlay(Owner.Character.Spells.CreateSpell(DEFAULT_ACTION, Owner, Owner));
+        }
+
+        protected abstract IList<Func<bool>> SetupPriorityActions();
+
+        protected Func<bool> CastOnRandom(SpellBook sb) {
+            return CastOnTargetMeetingCondition(sb, c => true);
+        }
+
+        protected Func<bool> CastOnTargetMeetingCondition(SpellBook sb, Func<Character, bool> requirement) {
+            return () => {
+                Character specificTarget =
+                    sb
+                    .TargetType
+                    .GetTargets(Owner.Character, battle)
+                    .Where(
+                        c => sb.IsCastable(Owner, new SpellParams(c)) && requirement(c))
+                    .PickRandom();
+
+                if (specificTarget != null) {
+                    handlePlay(Owner.Spells.CreateSpell(sb, Owner, new SpellParams(specificTarget)));
+                    return true;
+                }
+                return false;
+            };
+        }
+    }
+}
