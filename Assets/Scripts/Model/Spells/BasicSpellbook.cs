563df878221fabd4d6e4b7d6dae1bed9b97876d9
Atra <sharr@derpymail.org>
2017-07-19 06:03:27 -0400

    added new constructor for indicating priority

--- Assets/Scripts/Model/Spells/BasicSpellbook.cs
+++ Assets/Scripts/Model/Spells/BasicSpellbook.cs
@@ -8,6 +8,8 @@ namespace Scripts.Model.Spells {
     public abstract class BasicSpellbook : SpellBook {
         public BasicSpellbook(string spellName, Sprite sprite, TargetType target, SpellType spell) : base(spellName, sprite, target, spell, 0, 0, "Perform") { }
 
+        public BasicSpellbook(string spellName, Sprite sprite, TargetType target, SpellType spell, int priority) : base(spellName, sprite, target, spell, 0, 0, priority, "Perform") { }
+
         protected sealed override bool IsMeetOtherCastRequirements(SpellParams caster, SpellParams target) {
             return caster.Stats.State == Characters.State.ALIVE && target.Stats.State == Characters.State.ALIVE && IsMeetCastRequirements(caster, target);
         }
