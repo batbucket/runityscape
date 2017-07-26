34b4fc51d741bfc7a5402701bbec73beea31de1d
Atra <sharr@derpymail.org>
2017-07-26 17:42:48 -0400

    places can no longer be visited if all party members are dead

--- Assets/Scripts/Game/PlacePages/PlacePages.cs
+++ Assets/Scripts/Game/PlacePages/PlacePages.cs
@@ -23,7 +23,7 @@ namespace Scripts.Game.Pages {
 
         private void SetupRoot() {
             Page p = Root;
-            p.Condition = () => flags.Time < TimeOfDay.NIGHT;
+            p.Condition = PageUtil.GetVisitProcessCondition(flags, party);
             p.Icon = Util.GetSprite("walking-boot");
             p.Body = "Where would you like to go?";
             p.AddCharacters(Side.LEFT, party);
