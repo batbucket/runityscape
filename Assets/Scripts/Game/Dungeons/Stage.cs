2d4a6567f90184afa0dc12d1db8ab43dae9d820d
Andrew X <sharr@derpymail.org>
2017-09-03 11:11:19 -0400

    added for easier stage creation

---
+++ Assets/Scripts/Game/Dungeons/Stage.cs
@@ -0,0 +1,19 @@
+﻿using Scripts.Model.Pages;
+using System;
+
+namespace Scripts.Game.Dungeons {
+    public class Stage {
+        public readonly string StageName;
+        public readonly Func<Encounter[]> Encounters;
+
+        public Stage(string stageName, Func<Encounter[]> encounters) {
+            this.StageName = stageName;
+            this.Encounters = encounters;
+        }
+
+        public Stage() {
+            this.StageName = "Placeholder";
+            this.Encounters = () => new Encounter[] { new Encounter(Music.NORMAL) };
+        }
+    }
+}
