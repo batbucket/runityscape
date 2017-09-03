49fa06dc516823b89028baccf3fa89a7d71f8861
Andrew X <sharr@derpymail.org>
2017-09-03 11:11:32 -0400

    added incomplete world selection page

---
+++ Assets/Scripts/Game/Camp/WorldPages.cs
@@ -0,0 +1,10 @@
+﻿using Scripts.Model.Pages;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Scripts.Game.Pages {
+    public class WorldPages : PageGroup {
+        public WorldPages(Page head) : base(head) {
+        }
+    }
+}
