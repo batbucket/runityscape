cf7400acbdb69f992781e4abb635c96ce31ed6aa
Andrew X <sharr@derpymail.org>
2017-09-08 00:55:56 -0400

    add using

--- Assets/Scripts/Model/Page/Page.cs
+++ Assets/Scripts/Model/Page/Page.cs
@@ -4,6 +4,7 @@ using Scripts.Model.Processes;
 using Scripts.Model.TextBoxes;
 using Scripts.View.ObjectPool;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
