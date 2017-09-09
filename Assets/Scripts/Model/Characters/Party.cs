cebb6a38a9705d054c9a1a036133ad8207211db7
Andrew X <sharr@derpymail.org>
2017-09-08 20:19:03 -0400

    added comments! 10000 more classes to go!

--- Assets/Scripts/Model/Characters/Party.cs
+++ Assets/Scripts/Model/Characters/Party.cs
@@ -1,10 +1,5 @@
-﻿using Scripts.Game.Defined.Serialized.Items.Consumables;
-using Scripts.Game.Defined.Serialized.Items.Equipment;
-using Scripts.Game.Defined.Serialized.Items.Misc;
-using Scripts.Model.Buffs;
-using Scripts.Model.SaveLoad;
+﻿using Scripts.Model.SaveLoad;
 using Scripts.Model.SaveLoad.SaveObjects;
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -18,41 +13,67 @@ namespace Scripts.Model.Characters {
     /// being added to a party.
     /// </summary>
     public class Party : IEnumerable<Character>, ISaveable<PartySave> {
+
+        /// <summary>
+        /// This being a set prevents multiples of the same character from being added.
+        /// </summary>
         private HashSet<Character> members;
+
+        /// <summary>
+        /// Shared inventory among all members.
+        /// </summary>
         private Inventory shared;
 
+        /// <summary>
+        /// Construct a party with empty fields.
+        /// </summary>
         public Party() {
             this.members = new HashSet<Character>(new IdNumberEqualityComparer<Character>());
             this.shared = new Inventory();
-            shared.Add(new Apple());
-            shared.Add(new Apple());
-            shared.Add(new BrokenSword());
-            shared.Add(new BrokenSword());
         }
 
+        /// <summary>
+        /// Get the shared inventory.
+        /// </summary>
         public Inventory Shared {
             get {
                 return shared;
             }
         }
 
+        /// <summary>
+        /// Get the default character, good if you need any party member for something.
+        /// </summary>
         public Character Default {
             get {
                 return members.FirstOrDefault();
             }
         }
 
+        /// <summary>
+        /// Grabbing the party members should be done through this,
+        /// since it cannot be modified.
+        /// </summary>
         public ICollection<Character> Collection {
             get {
                 return new ReadOnlyCollection<Character>(members.ToList());
             }
         }
 
-        public void AddMember(Character c) {
-            this.members.Add(c);
-            c.Inventory = Shared;
+        /// <summary>
+        /// Adds a member and makes its inventory
+        /// reference the shared one.
+        /// </summary>
+        /// <param name="partyMemberToAdd"></param>
+        public void AddMember(Character partyMemberToAdd) {
+            this.members.Add(partyMemberToAdd);
+            partyMemberToAdd.Inventory = Shared;
         }
 
+        /// <summary>
+        /// Converts the party into a serializable object.
+        /// </summary>
+        /// <returns>A serializable object representing the state of the party.</returns>
         public PartySave GetSaveObject() {
             InventorySave inventory = Shared.GetSaveObject();
             List<CharacterSave> mems = members.Select(c => c.GetSaveObject()).ToList();
@@ -68,14 +89,14 @@ namespace Scripts.Model.Characters {
                     // Search party list to see if any party member is the caster
                     for (int j = 0; j < mems.Count && !isPartyMemberFound; j++) {
 
-                        // Id match -- Party ref mode
+                        // Id match -- Ref party member mode: we want to reference the member again on Load.
                         if (mems[j].Id.Equals(pb.CasterCharacterId)) {
                             pb.SetupAsCasterInParty(j);
                             isPartyMemberFound = true;
                         }
                     }
 
-                    // No member found, -- Use stat values mode
+                    // Caster is not a party member -- Spoof stats mode: we want to reference the caster's stats only to avoid infinite loops.
                     if (!isPartyMemberFound) {
                         pb.SetupAsCasterNotInParty();
                     }
@@ -85,6 +106,10 @@ namespace Scripts.Model.Characters {
             return new PartySave(mems, inventory);
         }
 
+        /// <summary>
+        /// Setup the PartySave from the serialized object.
+        /// </summary>
+        /// <param name="saveObject">Save object representing the state of the party.</param>
         public void InitFromSaveObject(PartySave saveObject) {
             List<Character> memList = new List<Character>();
 
@@ -110,15 +135,25 @@ namespace Scripts.Model.Characters {
                 c.Buffs.InitFromSaveObject(saveObject.Characters[i].Buffs); // Assumes matching indices
             }
 
+            // Add members
             foreach (Character c in memList) {
                 members.Add(c);
             }
         }
 
-        public void RemoveMember(Character c) {
-            members.Remove(c);
+        /// <summary>
+        /// Removes the character from the party.
+        /// </summary>
+        /// <param name="memberToRemove">Character to be removed.</param>
+        public void RemoveMember(Character memberToRemove) {
+            members.Remove(memberToRemove);
         }
 
+        /// <summary>
+        /// Checks if characters are equal and inventories.
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
         public override bool Equals(object obj) {
             var item = obj as Party;
 
@@ -131,6 +166,10 @@ namespace Scripts.Model.Characters {
             return this.Shared.Equals(item.Shared) && thisSet.SetEquals(thatSet);
         }
 
+        /// <summary>
+        /// Party has no hashable content since every element can change.
+        /// </summary>
+        /// <returns>Hash</returns>
         public override int GetHashCode() {
             return 0;
         }
