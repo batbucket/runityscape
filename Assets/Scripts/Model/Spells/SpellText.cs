0e56b06d7be4d5b1e9aa37be7f24de587addafd5
Andrew X <sharr@derpymail.org>
2017-09-08 23:47:46 -0400

    remove unused classes

--- Assets/Scripts/Model/Spells/SpellText.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class SpellText {
-    public readonly string Name;
-    public readonly string Description;
-}
