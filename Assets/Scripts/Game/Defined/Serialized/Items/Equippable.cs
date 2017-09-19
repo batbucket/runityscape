a808bbfb1835adeab9899945d1f8a33b3a7a05e4
Andrew X <sharr@derpymail.org>
2017-09-18 22:47:28 -0400

    new shield item

--- Assets/Scripts/Game/Defined/Serialized/Items/Equippable.cs
+++ Assets/Scripts/Game/Defined/Serialized/Items/Equippable.cs
@@ -4,7 +4,9 @@ using Scripts.Model.Items;
 using Scripts.Model.Stats;
 
 namespace Scripts.Game.Defined.Serialized.Items {
+
     public class PoisonArmor : EquippableItem {
+
         public PoisonArmor() : base(EquipType.ARMOR, 10, "Poisoned Armor", "This doesn't look safe.") {
             Stats.Add(StatType.VITALITY, 3);
             Stats.Add(StatType.AGILITY, -1);
@@ -13,10 +15,10 @@ namespace Scripts.Game.Defined.Serialized.Items {
         public override Buff CreateBuff() {
             return new Poison();
         }
-
     }
 
     public class BrokenSword : EquippableItem {
+
         public BrokenSword() : base(EquipType.WEAPON, 5, "Broken Sword", "A broken sword dropped by a spirit.") {
             Stats.Add(StatType.STRENGTH, 1);
             Stats.Add(StatType.VITALITY, -4);
@@ -24,6 +26,7 @@ namespace Scripts.Game.Defined.Serialized.Items {
     }
 
     public class GhostArmor : EquippableItem {
+
         public GhostArmor() : base(EquipType.ARMOR, 10, "Cursed Mail", "A cursed chainmail dropped by a spirit.") {
             Stats.Add(StatType.STRENGTH, -10);
             Stats.Add(StatType.AGILITY, -10);
@@ -31,10 +34,12 @@ namespace Scripts.Game.Defined.Serialized.Items {
     }
 
     public class Shield : EquippableItem {
+
         public Shield() : base(Util.GetSprite("round-shield"), EquipType.OFFHAND, 0, "Basic Shield ", "A basic wooden shield.") {
             Stats.Add(StatType.AGILITY, -10);
             Stats.Add(StatType.VITALITY, 10);
         }
+
         public override Buff CreateBuff() {
             return new DamageResist();
         }
