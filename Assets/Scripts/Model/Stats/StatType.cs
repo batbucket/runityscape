b47f2bdbcc54fef345ece1a4354e2a6f42713ce5
Andrew X <sharr@derpymail.org>
2017-09-22 15:33:31 -0400

    mana now uses water drop as icon

--- Assets/Scripts/Model/Stats/StatType.cs
+++ Assets/Scripts/Model/Stats/StatType.cs
@@ -66,7 +66,7 @@ namespace Scripts.Model.Stats {
 
         public static readonly StatType MANA = new StatType(BoundType.RESOURCE,
                                                                     "Mana",
-                                                                    "fox-head",
+                                                                    "water-drop",
                                                                     "Magical resources.",
                                                                     Color.blue);
 
