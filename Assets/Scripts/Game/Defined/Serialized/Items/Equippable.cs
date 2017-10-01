08f2a995d5c0614e70572774c39f0f64af6ca1a8
Andrew X <me@andrewxue.com>
2017-09-30 23:56:10 -0400

    moved some equipment into separate file

--- Assets/Scripts/Game/Defined/Serialized/Items/Equippable.cs
+++ Assets/Scripts/Game/Defined/Serialized/Items/Equippable.cs
@@ -18,22 +18,6 @@ namespace Scripts.Game.Defined.Serialized.Items {
         }
     }
 
-    public class BrokenSword : EquippableItem {
-
-        public BrokenSword() : base(EquipType.WEAPON, 5, "Broken Sword", "A broken sword dropped by a spirit.") {
-            AddFlatStatBonus(StatType.STRENGTH, 1);
-            AddFlatStatBonus(StatType.VITALITY, -4);
-        }
-    }
-
-    public class GhostArmor : EquippableItem {
-
-        public GhostArmor() : base(EquipType.ARMOR, 10, "Cursed Mail", "A cursed chainmail dropped by a spirit.") {
-            AddFlatStatBonus(StatType.STRENGTH, -10);
-            AddFlatStatBonus(StatType.AGILITY, -10);
-        }
-    }
-
     public class Shield : EquippableItem {
 
         public Shield() : base(Util.GetSprite("round-shield"), EquipType.OFFHAND, 0, "Basic Shield ", "A basic wooden shield.") {
---
+++ Assets/Scripts/Game/Defined/Serialized/Items/RuinsItems.cs
@@ -0,0 +1,21 @@
+﻿using Scripts.Model.Items;
+using Scripts.Model.Stats;
+
+namespace Scripts.Game.Defined.Serialized.Items {
+
+    public class BrokenSword : EquippableItem {
+
+        public BrokenSword() : base(EquipType.WEAPON, 5, "Broken Sword", "A broken sword dropped by a spirit.") {
+            AddFlatStatBonus(StatType.STRENGTH, 1);
+            AddFlatStatBonus(StatType.VITALITY, -4);
+        }
+    }
+
+    public class GhostArmor : EquippableItem {
+
+        public GhostArmor() : base(EquipType.ARMOR, 10, "Cursed Mail", "A cursed chainmail dropped by a spirit.") {
+            AddFlatStatBonus(StatType.STRENGTH, -10);
+            AddFlatStatBonus(StatType.AGILITY, -10);
+        }
+    }
+}
