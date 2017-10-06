99b4a07bc89627fb32c95ca8be8eab1ef41cc256
Andrew X <me@andrewxue.com>
2017-10-06 01:17:34 -0400

    close #72

--- Assets/Scripts/Model/Spells/TargetType.cs
+++ Assets/Scripts/Model/Spells/TargetType.cs
@@ -21,13 +21,13 @@ namespace Scripts.Model.Spells {
         }
 
         public static TargetType SELF = new TargetType("Self", false, (c, p) => new Character[] { c });
-        public static TargetType SINGLE_ALLY = new TargetType("One ally", false, (c, p) => p.GetAllies(c).Where(a => a.Stats.State == State.ALIVE).ToArray());
+        public static TargetType SINGLE_ALLY = new TargetType("One ally", false, (c, p) => p.GetAllies(c));
 
         //public static TargetType ALL_ALLIES = new TargetType("All allies", (c, p) => p.GetAllies(c).Where(a => a.Stats.State == State.ALIVE).ToArray());
-        public static TargetType SINGLE_ENEMY = new TargetType("One enemy", true, (c, p) => p.GetFoes(c).Where(a => a.Stats.State == State.ALIVE).ToArray());
+        public static TargetType SINGLE_ENEMY = new TargetType("One enemy", true, (c, p) => p.GetFoes(c));
 
         //public static TargetType ALL_ENEMIES = new TargetType("All enemies", (c, p) => p.GetFoes(c).Where(a => a.Stats.State == State.ALIVE).ToArray());
-        public static TargetType ANY = new TargetType("Any", true, (c, p) => p.GetAll().Where(a => a.Stats.State == State.ALIVE).ToArray());
+        public static TargetType ANY = new TargetType("Any", true, (c, p) => p.GetAll());
 
         //public static TargetType ALL = new TargetType("All", (c, p) => p.GetAll().Where(a => a.Stats.State == State.ALIVE).ToArray());
         public static TargetType NONE = new TargetType("None", false, (c, p) => new Character[0]);
