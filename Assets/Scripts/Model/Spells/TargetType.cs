67b2cfca87449f47ee9d8690de48a0f1cab00dc2
Andrew X <me@andrewxue.com>
2017-10-08 13:58:44 -0400

    add enum for stating whether the targetType can hit multiple targets

--- Assets/Scripts/Model/Spells/TargetType.cs
+++ Assets/Scripts/Model/Spells/TargetType.cs
@@ -12,30 +12,37 @@ namespace Scripts.Model.Spells {
     public sealed class TargetType {
         public readonly string Name;
         public readonly bool IsTargetEnemies;
+        public readonly TargetCount TargetCount;
         private Func<Character, Page, ICollection<Character>> getFunc;
 
-        private TargetType(string name, bool isTargetEnemies, Func<Character, Page, ICollection<Character>> getFunc) {
+        private TargetType(string name, TargetCount targetCount, Func<Character, Page, ICollection<Character>> getFunc) {
             this.Name = name;
-            this.IsTargetEnemies = isTargetEnemies;
+            this.TargetCount = targetCount;
             this.getFunc = getFunc;
         }
 
-        public static TargetType SELF = new TargetType("Self", false, (c, p) => new Character[] { c });
-        public static TargetType SINGLE_ALLY = new TargetType("One ally", false, (c, p) => p.GetAllies(c));
+        public static TargetType SELF = new TargetType("Self", TargetCount.SINGLE_TARGET, (c, p) => new Character[] { c });
+        public static TargetType SINGLE_ALLY = new TargetType("One ally", TargetCount.SINGLE_TARGET, (c, p) => p.GetAllies(c));
 
         //public static TargetType ALL_ALLIES = new TargetType("All allies", (c, p) => p.GetAllies(c).Where(a => a.Stats.State == State.ALIVE).ToArray());
-        public static TargetType SINGLE_ENEMY = new TargetType("One enemy", true, (c, p) => p.GetFoes(c));
+        public static TargetType SINGLE_ENEMY = new TargetType("One enemy", TargetCount.SINGLE_TARGET, (c, p) => p.GetFoes(c));
 
         //public static TargetType ALL_ENEMIES = new TargetType("All enemies", (c, p) => p.GetFoes(c).Where(a => a.Stats.State == State.ALIVE).ToArray());
-        public static TargetType ANY = new TargetType("Any", true, (c, p) => p.GetAll());
+        public static TargetType ANY = new TargetType("Any", TargetCount.SINGLE_TARGET, (c, p) => p.GetAll());
 
         //public static TargetType ALL = new TargetType("All", (c, p) => p.GetAll().Where(a => a.Stats.State == State.ALIVE).ToArray());
-        public static TargetType NONE = new TargetType("None", false, (c, p) => new Character[0]);
+        public static TargetType NONE = new TargetType("None", TargetCount.NONE, (c, p) => new Character[0]);
 
         //public static HashSet<TargetType> SINGLE_TARGET_OPTIONS = new HashSet<TargetType>(new IdentityEqualityComparer<TargetType>()) { SELF, SINGLE_ALLY, SINGLE_ENEMY, ANY };
 
         public ICollection<Character> GetTargets(Character caster, Page p) {
             return getFunc.Invoke(caster, p);
         }
+    }
+
+    public enum TargetCount {
+        MULTIPLE_TARGETS,
+        SINGLE_TARGET,
+        NONE
     }
 }
