a572ae0ff40b746ebfec6e2a13353a8c9b0ea696
Andrew X <me@andrewxue.com>
2017-10-11 22:23:36 -0400

    add oceanitems file

--- Assets/Scripts/Game/Defined/Serialized/Items/Equippable.cs
+++ Assets/Scripts/Game/Defined/Serialized/Items/Equippable.cs
@@ -29,17 +29,4 @@ namespace Scripts.Game.Defined.Serialized.Items {
             return new DamageResist();
         }
     }
-
-    public class FishHook : EquippableItem {
-
-        public FishHook() : base(EquipType.WEAPON, 50, "Fish Hook", "A used fish hook.") {
-            AddFlatStatBonus(StatType.STRENGTH, 5);
-            AddFlatStatBonus(StatType.AGILITY, 1);
-            AddFlatStatBonus(StatType.VITALITY, -1);
-        }
-
-        public override Buff CreateBuff() {
-            return new FishShook();
-        }
-    }
 }
---
+++ Assets/Scripts/Game/Defined/Serialized/Items/OceanItems.cs
@@ -0,0 +1,20 @@
+﻿using Scripts.Game.Defined.Serialized.Buffs;
+using Scripts.Model.Buffs;
+using Scripts.Model.Items;
+using Scripts.Model.Stats;
+
+namespace Scripts.Game.Defined.Serialized.Items {
+
+    public class FishHook : EquippableItem {
+
+        public FishHook() : base(EquipType.WEAPON, 50, "Fish Hook", "A used fish hook.") {
+            AddFlatStatBonus(StatType.STRENGTH, 5);
+            AddFlatStatBonus(StatType.AGILITY, 1);
+            AddFlatStatBonus(StatType.VITALITY, -1);
+        }
+
+        public override Buff CreateBuff() {
+            return new FishShook();
+        }
+    }
+}
